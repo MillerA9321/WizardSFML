91fd6e453ee75fb25b552638656513742f70fb76
admin <admin@BCS-Ubuntu-03.lrccd.ad.losrios.edu>
2022-12-05 03:59:44 -0800

    Edits are just some comments on what I plan to do today

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -5,3 +5,10 @@
 #include "Entity.h"
 #include "Engine.h"
 #pragma once
+
+
+//I'm sorry that I wasn't able to work on the project this weekend, but I have all day on Monday and Tuesday to work on it
+//Also did the professor make any comments about our UML Diagram? Unfortunately I screwed up on turning it in and the only comment I got was that it would not open.
+//I'll finish up the Entity class tomorrow and try to make atleast our textures behave and print out as they are supposed to on screen by Monday
+//If we get stuck on anything I could always go to his Office Hours on Tuesday because it is right after my Chem Class
+//Hopefully everything goes smoothly tomorrow and I don't run into a million errors
