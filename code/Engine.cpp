7230f0434c3ab8a67c856b1c870932c417213c6f
admin <admin@BCS-Ubuntu-21.lrccd.ad.losrios.edu>
2022-12-06 02:58:24 -0800

    Comments on what I plan to do Tuesday

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -7,14 +7,16 @@
 #pragma once
 
 
-//I'm sorry that I wasn't able to work on the project this weekend, but I have all day on Monday and Tuesday to work on it
-//Also did the professor make any comments about our UML Diagram? Unfortunately I screwed up on turning it in and the only comment I got was that it would not open.
-//I'll finish up the Entity class tomorrow and try to make atleast our textures behave and print out as they are supposed to on screen by Monday
-//If we get stuck on anything I could always go to his Office Hours on Tuesday because it is right after my Chem Class
-//Hopefully everything goes smoothly tomorrow and I don't run into a million errors
-
-//The only comment on the UML he made was
-//"One issue, in general you don't need to talk about code specifics (classes, etc)
-//in your requirements, just stick to the functionality."
-
+//I have not been able to figure out why the arena or the player is not centered yet
+//For me I do not see the player in linux but I do see the player when I run it on windows studios
+//It is stuck at the upperleft corner 
+//On Tuesday, I will wrap up making the slime vectors and make the score board and health bar show up
+//I'll try to make a Game Over title screen as well
+//I'll make a mana potion sprite as well since it looks like you added a reloading feature
+//I tried to make a custom background work but it end up all wonky so I will look into that today
+//I found out why the sprite are all so tiny and fixed the Green slime and king slime textures so far so I will wrap those up tomorrow as well
+//Hopefully I can get more done Tuesday than Monday because I've mainly just stared at zombies
+//trying to figure out why things are centering and how I plan to implement the slimes
+//Right now, instead of a leveling up feature I am going more on a more "till you die" type of game
+//so each slime spawns at a different interval of time, like green slime: 5 seconds, red slime: 10 seconds
 **/
