3fe68923f1784df0131843c9b026588def419614
JustinJune2 <w1964059@apps.losrios.edu>
2022-12-07 06:35:43 -0800

    Window Studio

--- code/wizardBattle.cpp
+++ code/wizardBattle.cpp
@@ -313,23 +313,24 @@ int main()
 			}
 		}
 
+		//If a projetile hits a slime
 		if (state == State::PLAYING)
 		{
 			for (int i = 0; i < 50; i++)
 			{
 				for (int j = 0; j < 50; j++)
 				{
-					if(projectiles->isInFlight() && slimes[j].isAlive())
+					if (projectiles->isInFlight() && slimes[j].isAlive())
 					{
-						if(projectiles[i].getPosition().intersects(slimes[j].getPosition()))
+						if (projectiles[i].getPosition().intersects(slimes[j].getPosition()))
 						{
 							projectiles[i].stop();
 						}
 					}
 				}
 			}
-		}
 
+		}
 		if (state == State::LEVELING_UP)
 		{
 		}
